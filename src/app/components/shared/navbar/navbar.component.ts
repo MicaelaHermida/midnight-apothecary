a4553e565aa4b0b7e0b9de7f9e8fe0fd039ab4ed
Mica <hermidatome.micaela@gmail.com>
2023-11-05 20:40:35 -0300

    navbar base

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  isLogged: boolean = false;
+  isAdminRole: boolean = false;
+  firebaseAuthenticationReady : boolean = false;
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+
+  async ngOnInit(): Promise<void> {
+    await this.authenticationService.waitForFirebaseAuthentication();
+    this.firebaseAuthenticationReady = true;
+    this.isLogged = await this.authenticationService.isUserLoggedIn();
+    this.isAdminRole = await this.authenticationService.getCurrentUserRole() === "admin";
+  }
+
+  async cerrarSesion(){
+    await this.authenticationService.logout();
+    this.router.navigate(['/home']);
+  }
 }
