1adee65b92207189f3564bf9c6229cad65edf3e7
Mica <hermidatome.micaela@gmail.com>
2024-05-09 16:04:42 -0300

    environment.example file

---
+++ src/app/environment/environment.example.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    API_KEY: 'your api key here',
+
+    PROJECT_API_KEY: "AIzaSyATu1q9xijreEeDRChL_u7mSDXibkuLHXc",
+    AUTH_DOMAIN: "midnight-apothecary.firebaseapp.com",
+    PROJECT_ID: "midnight-apothecary",
+    STORAGE_BUCKET: "midnight-apothecary.appspot.com",
+    MESSAGING_SENDER_ID: "645627604949",
+    APP_ID: "1:645627604949:web:2ed2e0e6a4522bf60c175c"
+}
